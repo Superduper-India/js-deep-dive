284b59be2749202c3fbdc590823d892026ab5b9e
Superduper-India <89244209+Superduper-India@users.noreply.github.com>
2022-12-02 22:52:03 +0900

    상속과 프로토타입

--- chapter19/example.test.js
+++ chapter19/example.test.js
@@ -38,6 +38,56 @@ function example() {
   console.log(circle.getArea());      // 78.53981633974483
 }
 
+// 19.2 상속과 프로토타입
+function example2() {
+  // 상속은 객체지향 프로그래밍의 핵심으로,
+  // 어떤 객체의 "프로퍼티" 또는 "메서드"를 다른 객체가 상속받아 그대로 사용할 수 있는 것을 말한다.
+
+  // Circle 생성자 함수가 생성하는 모든 객체(인스턴스)는 radius프로퍼티와 getArea메서드를 갖는다.
+  // 프로퍼티는 일반적으로 인스턴스마다 값이 다르지만, 메서드는 내용이 같으므로,
+  // 단 하나만 생성해서 모든 인스턴스가 공유하는게 바람직하다.
+  function Circle(radius) {
+    console.log(radius)
+    this.radius = radius;
+    this.getArea = function () {
+      // Math.PI는 원주율을 나타내는 상수다.
+      return Math.PI * this.radius ** 2;
+    };
+  }
+
+  // Circle 생성자 함수로 반지름이 1인 인스턴스 생성
+  const circle1 = new Circle(1);
+  // Circle 생성자 함수로 반지름이 2인 인스턴스 생성
+  const circle2 = new Circle(2);
+
+  // 동일한 Circle 생성자 함수에 의해 생성된 모든 인스턴스가
+  // getArea 메서드를 중복 생성하고 모든 인스턴스가 중복 소유한다.
+  // 때문에 불필요하게 메모리를 낭비하고, 퍼포먼스에 악영향을 준다.
+  console.log(circle1.getArea === circle2.getArea); // false, 즉 메서드가 중복으로 생성됐단 뜻
+
+  // js의 프로토타입을 기반으로 상속을 통해 불필요한 중복을 제거해보자.
+  function Solution(radius) {
+    this.radius = radius;
+  }
+
+  // Solution 생성자 함수가 생성한 모든 인스턴스가 getArea 메서드를
+  // 공유해서 사용할 수 있도록 프로토타입에 추가한다.
+  // 프로토타입은 Solution 생성자 함수의 prototype 프로퍼티에 바인딩되어 있다.
+  Solution.prototype.getArea = function () {
+    return Math.PI * this.radius ** 2;
+  };
+
+  // 인스턴스 생성
+  const solution1 = new Solution(1);
+  const solution2 = new Solution(2);
+
+  // Solution 생성자 함수가 생성한 모든 인스턴스는 부모 객체의 역할을 하는
+  // 프로토타입 Solution.prototype으로부터 getArea 메서드를 상속받는다.
+  // 즉, Solution 생성자 함수가 생성하는 모든 인스턴스는 하나의 getArea 메서드를 공유한다.
+  console.log(solution1.getArea === solution2.getArea); // true
+}
+
 test("run", () => {
-  expect(example()).toBe();
+  // expect(example()).toBe();
+  expect(example2()).toBe();
 });
